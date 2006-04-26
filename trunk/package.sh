e4d99b750f2ddece05cce0150d25b4a023008617
Anthony Lieuallen <arantius@gmail.com>
2006-04-26 00:45:04 +0000

    -
    git-svn-id: svn://localhost/extension/tabcontrol@67 28f79fa8-d1f5-0310-81f3-f5ca3e6eca6b

--- trunk/package.sh
+++ trunk/package.sh
@@ -1,8 +1,9 @@
 #!/bin/sh
-rm -f tabcontrol*.xpi
+PROJ=`sed '2,$d' chrome.manifest|awk '{print $2}'`
+rm -f ${PROJ}*.xpi
 VER=`grep 'em:version' install.rdf | sed 's/[^0-9.]//g'`
 
-echo CREATING: tabcontrol-${VER}.xpi
+echo CREATING: ${PROJ}-${VER}.xpi
 
-zip -9 tabcontrol-${VER}.xpi \
+zip -9 ${PROJ}-${VER}.xpi \
 	`find . -type d -name .svn -prune -false -o -type f -a -not -name package.sh`
