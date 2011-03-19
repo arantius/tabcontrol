081eef0304dae6b066a7e39037f9336e5ed60e42
Anthony Lieuallen <arantius@gmail.com>
2011-03-19 18:39:59 +0000

    Fixes #406
    * Do not override browser preference defaults.
    
    git-svn-id: svn://localhost/extension/tabcontrol@693 28f79fa8-d1f5-0310-81f3-f5ca3e6eca6b

--- defaults/preferences/tabcontrol.js
+++ defaults/preferences/tabcontrol.js
@@ -2,8 +2,3 @@ pref("extensions.{39952c40-5197-11da-8cd6-0800200c9a66}.description", "chrome://
 pref("tabcontrol.focusLeftOnClose", true);
 pref("tabcontrol.posRightOnAdd", true);
 pref("tabcontrol.leftRightGroup", false);
-pref("browser.showQuitWarning", true);
-pref("browser.tabs.closeWindowWithLastTab", false);
-pref("browser.tabs.insertRelatedAfterCurrent", false);
-pref("browser.tabs.tabMinWidth", 30);
-pref("browser.tabs.tabMaxWidth", 350);
